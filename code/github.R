086390846319ba8986cfab114b997c63a1790346
Jinseob Kim <jinseob2kim@gmail.com>
2022-02-18 11:38:47 +0000

    git

--- code/github.R
+++ code/github.R
@@ -21,13 +21,8 @@
 usethis::create_github_token() 
 ## in case usethis version < 2.0.0: usethis::browse_github_token() (or even better: update usethis!)
 
-## set personal access token:
-credentials::set_github_pat("YourPAT")
-
-## or store it manually in '.Renviron':
-usethis::edit_r_environ()
-## store your personal access token with: GITHUB_PAT=xxxyyyzzz
-## and make sure '.Renviron' ends with a newline
+## copy and paste your token and press enter
+gitcreds::gitcreds_set()
 
 # ----------------------------------------------------------------------------
 
@@ -44,4 +39,5 @@ usethis::git_sitrep()
 ## 'Personal access token: '<found in env var>''
 
 # ----------------------------------------------------------------------------
+
 ## THAT'S IT!
