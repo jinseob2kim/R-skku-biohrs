e89f6239f12cbb80fb5b377de43ee787c75aafce
Jinseob Kim <jinseob2kim@gmail.com>
2022-04-11 09:59:48 +0000

    mid

---
+++ exam/mid.R
@@ -0,0 +1,16 @@
+####### Midterm ########
+
+## data URL: 문제 1-3 모두 이 데이터를 이용합니다.
+"https://raw.githubusercontent.com/jinseob2kim/R-skku-biohrs/main/data/example_g1e.csv"
+
+## R코드와 실행결과를 모두 메일로 보내주십시오. (panthree@skku.edu, yumin.kim@zarathu.com)
+
+
+## 데이터를 읽은 후
+## Problem 1:  "Q_" 로 시작하는 변수는 범주형(factor)으로, 나머지 변수는 숫자로 만들어라.
+
+
+## Problem 2: RN_INDI, HME_YYYYMM 제외한 모든 연속변수의 연도별 평균과 표준편차를 구하라.
+
+
+## Problem 3: 연도별 FBS의 Boxplot을 그리고 pptx로 저장하라.
